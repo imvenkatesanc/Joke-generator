133f7710b2e921186b22fccce4d9ea5ab71f6aa2
Venkatesan C <108179969+imvenkatesanc@users.noreply.github.com>
2023-03-25 22:19:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const options ={
 };
 const apiURL = "https://api.api-ninjas.com/v1/jokes?limit=1"
 async function getJoke(){
+  try{
     jokeElement.innerText = "Loading...";
     buttonElement.disabled = true;
     buttonElement.innerText = "Updating..."
@@ -17,6 +18,12 @@ async function getJoke(){
     buttonElement.disabled = false;
     buttonElement.innerText = "Another joke"
     jokeElement.innerText = data[0].joke;
+} catch(error){
+    jokeElement.innerText="No network, check your connection!";
+    buttonElement.disabled = false;
+    buttonElement.innerText = "Tell me a joke"
+    console.log(error);
+}
 }
 
 buttonElement.addEventListener("click",getJoke);
